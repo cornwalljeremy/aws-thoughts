5fcf1b551131eeae1591c953febcd69ce0721f39
Jeremy Cornwall <cornwall.jeremy@gmail.com>
2021-11-27 17:24:53 -0700

    backend

---
+++ backend/.aws-sam/build/getAllThoughtsFunction/src/handlers/get-all-thoughts.js
@@ -0,0 +1,26 @@
+// Import the DynamoDB service
+const dynamodb = require("aws-sdk/clients/dynamodb");
+const docClient = new dynamodb.DocumentClient();
+// Get the table name from environment variable
+const tableName = process.env.TABLE_NAME;
+
+exports.getAllThoughtsHandler = async (event) => {
+  // assign the environment variable from the SAM template
+  const params = {
+    TableName: tableName,
+  };
+
+  const { Items } = await docClient.scan(params).promise();
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(Items),
+  };
+
+  // All log statements are written to CloudWatch
+  console.info(
+    `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+  );
+
+  return response;
+};
---
+++ backend/.aws-sam/build/putThoughtFunction/src/handlers/get-by-user.js
@@ -0,0 +1,25 @@
+exports.getByUserHandler = async (event) => {
+  if (event.httpMethod !== "GET") {
+    throw new Error(
+      `getMethod only accept GET method, you tried: ${event.httpMethod}`
+    );
+  }
+  // All log statements are written to CloudWatch
+  console.info("received:", event);
+
+  // Get username from pathParameters from APIGateway because of `/{username}` in the template.yaml
+  const username = event.pathParameters.username;
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `get by ${username}`,
+    }),
+  };
+
+  // All log statements are written to CloudWatch
+  console.info(
+    `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+  );
+  return response;
+};
