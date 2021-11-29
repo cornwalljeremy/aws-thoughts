683bed1207b6a9401eaaaf7a6c39963d27e75ff5
Jeremy Cornwall <cornwall.jeremy@gmail.com>
2021-11-28 22:06:58 -0700

    No CORS Problems

--- backend/.aws-sam/build/getByUserFunction/src/handlers/get-by-user.js
+++ backend/.aws-sam/build/getByUserFunction/src/handlers/get-by-user.js
@@ -29,6 +29,11 @@ exports.getByUserHandler = async (event) => {
   // Form the response
   const response = {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+    },
     body: JSON.stringify(Items),
   };
 
--- backend/.aws-sam/build/putThoughtFunction/src/handlers/get-all-thoughts.js
+++ backend/.aws-sam/build/putThoughtFunction/src/handlers/get-all-thoughts.js
@@ -14,6 +14,11 @@ exports.getAllThoughtsHandler = async (event) => {
 
   const response = {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+    },
     body: JSON.stringify(Items),
   };
 
